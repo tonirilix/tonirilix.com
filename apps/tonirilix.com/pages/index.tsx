2a186bdfc043548d8825b292cbf6bd7bd26ea585
Jesus Montes <3756508+tonirilix@users.noreply.github.com>
2022-12-18 11:44:29 -0600

    feat: update tailwind config
    Exports some config to a global preset

--- apps/tonirilix.com/pages/index.tsx
+++ apps/tonirilix.com/pages/index.tsx
@@ -22,6 +22,25 @@ export function Index() {
           </a>
         </h1>
 
+        <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+          <div className="text-xl font-medium text-black">ChitChat</div>
+          <p className="text-slate-500">You have a new message!</p>
+        </div>
+
+        <article className="prose lg:prose-lg">
+          <h1>Garlic bread with cheese: What the science tells us</h1>
+          <p>
+            For years parents have espoused the health benefits of eating garlic
+            bread with cheese to their children, with the food earning such an
+            iconic status in our culture that kids will often dress up as warm,
+            cheesy loaf for Halloween.
+          </p>
+          <p>
+            But a recent study shows that the celebrated appetizer may be linked
+            to a series of rabies cases springing up around the country.
+          </p>
+        </article>
+
         <p className="mt-3 text-2xl">
           Get started by editing{' '}
           <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
--- apps/tonirilix.com/tailwind.config.js
+++ apps/tonirilix.com/tailwind.config.js
@@ -1,13 +1,16 @@
 const path = require('path');
+const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     path.join(__dirname, './pages/**/*.{js,ts,jsx,tsx}'),
     path.join(__dirname, './components/**/*.{js,ts,jsx,tsx}'),
+    ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
     extend: {},
   },
   plugins: [],
+  presets: [require('../../tailwind-workspace-preset')],
 };
