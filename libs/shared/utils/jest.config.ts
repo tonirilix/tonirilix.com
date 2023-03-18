83f6c2cfa1cc73aa47e2ceb7569029298fdc0d65
Jesus Montes <3756508+tonirilix@users.noreply.github.com>
2023-03-18 01:01:33 -0600

    feat(shared) add utils project

---
+++ libs/shared/utils/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'shared-utils',
+  preset: '../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/libs/shared/utils',
+};
